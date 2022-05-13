d880142c39c8efe00fa1b997cc564ca16ca11f1d
Tramnvph14967 <tramnvph14967@fpt.edu.vn>
2022-05-13 22:01:03 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductsComponent } from './components/products/products.component';
-
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
   { path: 'header', component: HeaderComponent},
-  { path: 'product', component: ProductsComponent}
+  { path: 'product', component: ProductsComponent},
+  { path: 'footer', component: FooterComponent}
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NumberSymbol } from '@angular/common';
 import { Component } from '@angular/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tramnvph14967';
-}
+  productName: string = "Product A";
+  productPrice: number = 1000;
+  productStatus: boolean = false;
+  productInfo: { id: number, price: number, name: string } = {
+    id: 1,
+    name: "product A",
+    price: 2000
+  };
+
+  productList: { id: number, price: number, name: string, status: boolean }[] = [
+    {
+      id: 1,
+      name: "product A",
+      price: 2000,
+      status: false
+    },
+    {
+      id: 2,
+      name: "product B",
+      price: 3000,
+      status: true
+    }
+  ]
+
+
+  onHandleClick() {
+    console.log("clickek");
+    this.productStatus = !this.productStatus
+  }
+
+  onHandleAdd(id:number, name:string, price: number, status: string) {
+    
+
+  }
+
+  onHandleDelete(id: number) {
+    alert("Bạn có muốn xóa sản phẩm hay không?");
+    this.productList = this.productList.filter(product => product.id !== id)
+    console.log(id);
+  }
+
+
+  onHandleKeyPress(event: any) {
+    console.log(event.target.value);
+    this.title = event.target.value;
+  }
+
+
+
+} 
