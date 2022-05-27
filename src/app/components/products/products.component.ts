f303b7b6d991a2db35a0dea701dccc57a5503265
Tramnvph14967 <tramnvph14967@fpt.edu.vn>
2022-05-27 22:37:32 +0700

    update

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,23 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IProduct } from '../../models/Product';
-
+import { ProductService } from '../../services/product.service'
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  @Input("data") productList !: IProduct[];
-  productDetail !: IProduct; // undefined
-
-  constructor() { }
+  productList!: IProduct[];
+  constructor(private productService: ProductService) {
+    this.productList = this.productService.getProducts();
+  }
 
   ngOnInit(): void {
   }
 
-
-  showDetail(id:number){
-
-    this.productDetail = this.productList.find(item => item.id === id)!;
-  }
 }
+// /product -> list
+// /product/:id -> detail
